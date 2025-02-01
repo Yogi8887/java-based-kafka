6ae5b780005c7895f7ac934aa563dacb462d31de
Yogi8887 <yogi.nit.mca@gmail.com>
2025-02-01 13:04:16 +0530

    refactor code

--- java/src/main/java/com/kafka/java/kafka/KafkaConsumer.java
+++ java/src/main/java/com/kafka/java/kafka/KafkaConsumer.java
@@ -1,16 +1,14 @@
 package com.kafka.java.kafka;
 
-import com.kafka.java.dto.response.JsonPlaceHolderResponse;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Service;
 
-import java.util.List;
 
 @Service
 public class KafkaConsumer {
 
     @KafkaListener(topics = "json-place-holder", groupId = "my-group")
-    public void listen(List<JsonPlaceHolderResponse> record) {
+    public void listen(String record) {
         System.out.println("Received Message: " + record);
     }
 }
